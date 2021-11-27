6a0b3de65af3baac5cc8710eb7dad40ec12b2d2a
GitpreNeuR <95102789+GitpreNeuR@users.noreply.github.com>
2021-11-27 18:25:53 +0530

    Update firebase.js

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -3,13 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARvT__jj5NqiJSPdQf5cxXpT2BctSn57k",
-  authDomain: "netflix-67cdb.firebaseapp.com",
-  projectId: "netflix-67cdb",
-  storageBucket: "netflix-67cdb.appspot.com",
-  messagingSenderId: "324765067332",
-  appId: "1:324765067332:web:db4c1371755595e7047411",
-  measurementId: "G-9RYVC1L1MJ"
+  YOUR_FIREBASE_API
 };
 
 firebase.initializeApp(firebaseConfig);
